95961283409baebab2d2bc61f0e94cf9694ecbf9
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-28 01:00:48 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 def reverse_each_word(string)
 
+<<<<<<< HEAD
    
 array = string.split
 final = []
@@ -14,6 +15,15 @@ array.collect do |element|
   end
    #I have to join them 
   final.join(" ")
+=======
+array = string.split
+
+final = array.each |element|
+    final.reverse
+
+  end
+
+>>>>>>> 5161d22a1ecc158faf459fcbffc1ddaa226eb381
 end 
 
 
